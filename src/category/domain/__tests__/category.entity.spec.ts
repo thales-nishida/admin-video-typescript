966f12c6d3b2115731806ad83b517ca9da37d845
Thales Nishida <nishida.tn@proton.me>
2025-07-29 20:12:04 +0000

    added uuid on category entity

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../category.entity";
 
 describe('Category Unit Tests', () => {
@@ -6,7 +7,7 @@ describe('Category Unit Tests', () => {
             const category = new Category({
                         name: 'Movie',
                     });
-            expect(category.category_id).toBeUndefined();
+            expect(category.category_id).toBeInstanceOf(Uuid);
             expect(category.name).toBe('Movie');
             expect(category.description).toBeNull();
             expect(category.is_active).toBeTruthy();
@@ -16,18 +17,31 @@ describe('Category Unit Tests', () => {
         test('should create a category with all properties', () => {
             const created_at = new Date();
             const category = new Category({
-                category_id: '123',
                 name: 'Movie',
                 description: 'some description',
                 is_active: false,
                 created_at,
             });
 
-            expect(category.category_id).toBe('123');
+            expect(category.category_id).toBeInstanceOf(Uuid);
             expect(category.name).toBe('Movie');
             expect(category.description).toBe('some description');
             expect(category.is_active).toBeFalsy();
             expect(category.created_at).toBe(created_at);
+        });
+    });
+
+    describe("category_id field", () => {
+        const arrange = [
+            { category_id: undefined},
+            { category_id: null },
+            { category_id: new Uuid() },
+        ];
+        test.each(arrange)('id: %j', ({ category_id }) => {
+            const category = new Category({
+                name: 'Movie',
+                category_id: category_id as any,
+            });
         });
     });
 
